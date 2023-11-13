f51ca90bc638baf6a48c9886d93e8102317b3635
Oscar Chen <oscar999@mail.ustc.edu.cn>
2023-11-13 21:24:00 +0800

    test

---
+++ chp2/basicGrammar.py
@@ -0,0 +1,13 @@
+# 多行语句
+number_one = 1
+number_two = 2
+number_three =3 
+total = number_one + \
+        number_two + \
+        number_three
+print (total)
+
+## [] {} () 中的多行语句，不需要使用反斜杠，直接换行
+total = ['1',
+         '2',
+         '3']
---
+++ chp3/tests/unit/test_sample.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_add2():    
+    assert add(1, 2) == 4
+
+
+
+    
